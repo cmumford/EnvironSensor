9f33e55490b1f2d05944014fc3b2a282fa4545c9
Chris Mumford <cmumford@cmumford.com>
2024-05-16 16:35:16 -0400

    Improve error reporting in generate-nvs.sh

--- pio/generate-nvs.sh
+++ pio/generate-nvs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
 if [[ -z "${ESP_IDF_VERSION}" ]]; then
   echo "ESP-IDF not loaded."
@@ -11,9 +11,21 @@ PARTITION_SIZE="0x6000"
 DEVICE="/dev/cu.usbserial-0001"
 
 NVS_PARTITION_GEN=${IDF_PATH}/components/nvs_flash/nvs_partition_generator/nvs_partition_gen.py
+rc=$?
+if [ $rc -ne 0 ]; then
+  echo "Error generating NVS partition."
+  exit $rc
+fi
 
 ${NVS_PARTITION_GEN} generate preferences.csv preferences.bin ${PARTITION_SIZE}
 
 parttool.py -p $DEVICE write_partition --partition-name=nvs --input=preferences.bin
+rc=$?
+if [ $rc -ne 0 ]; then
+  echo
+  echo "Error writing NVS partition."
+  echo "Check the VSCode serial monitor which may be interfering with this write."
+  exit $rc
+fi
 
 echo "NVS partition build/flash complete."
