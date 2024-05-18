ed993f455bb3fd0758c9cd552154deb8b46ba5e5
Chris Mumford <cmumford@cmumford.com>
2024-05-18 10:31:35 -0400

    Start of script to dump stored preferences

---
+++ pio/dump-nvs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ -z "${ESP_IDF_VERSION}" ]]; then
+  echo "ESP-IDF not loaded."
+  echo "Probably need to do something like:"
+  echo "source ~/esp/v5.1.2/esp-idf/export.sh"
+  exit 1
+fi
+
+DEVICE="/dev/cu.usbserial-0001"
+
+parttool.py -p $DEVICE read_partition --partition-name=nvs --output=nvs.bin
+rc=$?
+if [ $rc -ne 0 ]; then
+  echo
+  echo "Error writing NVS partition."
+  echo "Check the VSCode serial monitor which may be interfering with this write."
+  exit $rc
+fi
+
